942c77ecd8b21fa7025be6c453dc334d04b5ac25
Matthias K. Hoffmann <matthias.hoffmann@uni-saarland.de>
2022-08-18 11:03:24 +0200

    Die Funktion heißt isfield ;)

--- RunWaveexample.m
+++ RunWaveexample.m
@@ -5,20 +5,15 @@ Nfreq = 15; % How many frequencies to run
 
 IndWaves = zeros(t,Nfreq); 
 Waves = zeros(t,1);
+
 for i = 1:t
-
-
-[wave Individual] = SimplexStochasticWave(i,'N_freq',Nfreq,'Seed',2);
-IndWaves(i,:) = Individual;
-Waves(i) = wave;
+    [wave, Individual] = SimplexStochasticWave(i,'N_freq',Nfreq,'Seed',2);
+    IndWaves(i,:) = Individual;
+    Waves(i) = wave;
 end
-% figure
-% plot(IndWaves(:,2))
-figure
+
+figure(1)
 plot(Waves)
 %%
-figure
-for i = 1:Nfreq
-    plot(IndWaves(:,i))
-    hold on 
-end
+figure(2)
+plot(IndWaves)
